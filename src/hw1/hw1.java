d59d27a76b35636a123e9eacc8dd8ffbd4ba569c
user <user@EA05>
2020-09-29 09:41:44 +0800

    rename

--- src/hw1/hw1.java
+++ src/hw1/hw1.java
@@ -1,10 +1,12 @@
-package hw1;
+package lab1;
 
-public class hw1 {
+public class lab1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("I'm Ä¬®a¥°");
+		System.out.println("NID: D0886096");
+		System.out.println("This is lab 1 hw");
 	}
 
 }
